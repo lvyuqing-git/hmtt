48e697f2c4cc7848ee4251197d656c97f3116fb1
一打砖头拍不倒 <lyq15360562781@163.com>
2020-01-27 17:26:21 +0800

    配置路由显示login页面

--- hmtt/src/main.js
+++ hmtt/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './assets/reset.css'
+import router from '@/router/index'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+	router,
+	render: h => h(App)
 }).$mount('#app')
