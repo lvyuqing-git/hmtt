114a77e2fa8935bd026c0efc388c3671ec72382d
一打砖头拍不倒 <lyq15360562781@163.com>
2020-01-27 17:47:34 +0800

    login页面静态样式渲染

---
+++ hmtt/src/components/hmbutton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="button">
+      
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.button{
+    width: 100vw/360*318;
+    border: 1px solid black;
+    height: 550px;
+}
+</style>
--- hmtt/src/views/Login.vue
+++ hmtt/src/views/Login.vue
@@ -3,76 +3,28 @@
     <div class="container">
       <div class="close"><span class="iconfont iconicon-test"></span></div>
       <div class="logo"><span class="iconfont iconnew"></span></div>
-      <div class="inputs">
-     
-          <van-field v-model="username"
-                     error
-                     required
-                     label="用户名"
-                     placeholder="请输入用户名" />
-          <van-field v-model="password"
-                     required
-                     label="密码"
-                     placeholder="请输入密码"
-                     error-message="密码错误" />
-     
-      </div>
+      <div class="inputs"><input placeholder="请输入手机号"
+               class="input"><input placeholder="密码"
+               class="input"
+               type="password"></div>
       <p class="tips">
         没有账号？
         <a href="#/register"
            class="">去注册</a></p>
-      <div class="button">登录按钮</div>
+      <hmbutton></hmbutton>
     </div>
   </div>
 
 </template>
 
 <script>
+import hmbutton from '../components/hmbutton'
 export default {
-    data () {
-        return {
-            username : '',
-            password : ''
-
-        }
-    }
+  components: {
+    hmbutton
+  }
 }
 </script>
 
-<style lang='less' scoped>
-.container {
-  padding: 20px;
-}
-
-.close {
-  span {
-    font-size: 27 / 360 * 100vw;
-  }
-}
-
-.logo {
-  display: flex;
-  justify-content: center;
-
-  span {
-    display: block;
-    font-size: 126 / 360 * 100vw;
-    color: #d81e06;
-  }
-}
-
-.inputs {
-  input {
-    margin-bottom: 20px;
-  }
-}
-
-.tips {
-  text-align: right;
-  margin-bottom: 20px;
-
-  a {
-    color: #3385ff;
-  }
-}
+<style>
 </style>
