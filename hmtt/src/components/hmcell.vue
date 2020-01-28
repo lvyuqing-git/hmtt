7879501b9bef6761366144259ab2ee789ba81bdc
一打砖头拍不倒 <lyq15360562781@163.com>
2020-01-28 23:24:10 +0800

    封装cell栏

---
+++ hmtt/src/components/hmcell.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="cell">
+      <div class="left">{{title}}</div>
+      <div class="right">{{desc}}<van-icon name="arrow" /></div>
+      </div>
+</template>
+
+<script>
+export default {
+    props: ['title','desc']
+}
+</script>
+
+<style>
+.cell{
+    display: flex;
+    justify-content: space-between;
+    margin: 0 15px;
+    border-bottom: 1px solid #dbdbdb;
+    line-height: 50px;
+}
+.right{
+    font-size: 14px;
+    color: #a1a1a1
+}
+.van-icon{
+    font-size: 20px;
+    vertical-align: middle
+}
+</style>
