8133f3e26c3d0f8dd0d768d9fadcf98deed56d5b
一打砖头拍不倒 <lyq15360562781@163.com>
2020-01-28 16:46:25 +0800

    实现登录功能

--- hmtt/src/views/Login.vue
+++ hmtt/src/views/Login.vue
@@ -3,15 +3,25 @@
     <div class="container">
       <div class="close"><span class="iconfont iconicon-test"></span></div>
       <div class="logo"><span class="iconfont iconnew"></span></div>
-      <div class="inputs"><input placeholder="请输入手机号"
-               class="input"><input placeholder="密码"
-               class="input"
-               type="password"></div>
+      <div class="inputs">
+        <hminout placeholder="请输入账号"
+                 :value='user.username'
+                 @input='inputUser'
+                 msg='账号输入错误'
+                 :rule='/^\d{6}$/'
+                 ></hminout>
+        <hminout placeholder="请输入密码"
+                 v-model='user.password'
+                 msg='密码输入错误'
+                 :rule='/^\d{3}$/'></hminout>
+
+      </div>
       <p class="tips">
         没有账号？
         <a href="#/register"
            class="">去注册</a></p>
-      <hmbutton></hmbutton>
+      <hmbutton @click="btnClick"
+                type='primary'>登录</hmbutton>
     </div>
   </div>
 
@@ -19,12 +29,68 @@
 
 <script>
 import hmbutton from '../components/hmbutton'
+import hminout from '../components/hminput'
+import {login} from '../apis/apiUser'
 export default {
   components: {
-    hmbutton
+    hmbutton,
+    hminout
+  },
+  data() {
+    return {
+      user: {
+        username: '10086',
+        password: '123'
+      }
+    }
+  },
+  methods: {
+   async btnClick() {
+      let res = await login(this.user)
+      console.log(res)
+
+    },
+    inputUser(data) {
+      this.user.username = data
+    }
   }
 }
 </script>
 
-<style>
+<style lang='less' scpoed>
+.container {
+  padding: 20px;
+}
+
+.close {
+  span {
+    font-size: 27 / 360 * 100vw;
+  }
+}
+
+.logo {
+  display: flex;
+  justify-content: center;
+
+  span {
+    display: block;
+    font-size: 126 / 360 * 100vw;
+    color: #d81e06;
+  }
+}
+
+.inputs {
+  input {
+    margin-bottom: 20px;
+  }
+}
+
+.tips {
+  text-align: right;
+  margin-bottom: 20px;
+
+  a {
+    color: #3385ff;
+  }
+}
 </style>
