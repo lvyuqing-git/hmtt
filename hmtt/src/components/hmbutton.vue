d5a389d164f2858fb6b28f7065f192c5d66442bb
一打砖头拍不倒 <lyq15360562781@163.com>
2020-01-28 16:42:09 +0800

    封装输入框和按钮组件

--- hmtt/src/components/hmbutton.vue
+++ hmtt/src/components/hmbutton.vue
@@ -1,19 +1,36 @@
 <template>
-  <div class="button">
-      
+  <div class="button"
+       @click="btnClick"
+       :class="{primary : type == 'primary' , info:type == 'info'}">
+    <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
-
+  props: ['type'],
+  methods: {
+    btnClick(event) {
+      this.$emit('click', event)
+    }
+  }
 }
 </script>
 
-<style>
-.button{
-    width: 100vw/360*318;
-    border: 1px solid black;
-    height: 550px;
+<style lang='less' scoped>
+.button {
+  width: 318 * 100vw/360;
+  height: 50px;
+  text-align: center;
+  line-height: 50px;
+  border-radius: 25px;
+  font-size: 20px;
+  color: #fff;
+}
+.primary {
+  background-color: green;
+}
+.info {
+  background-color: red;
 }
 </style>
---
+++ hmtt/src/components/hminput.vue
@@ -0,0 +1,44 @@
+<template>
+  <input type="text"
+         @input="handleInput"
+         :class="{success : status,error : !status}">
+</template>
+
+<script>
+export default {
+  props: ['rule'],
+  data() {
+    return {
+      status: true
+    }
+  },
+  methods: {
+    handleInput(event) {
+      let value = event.target.value
+      if (this.rule && this.rule.test(value)) {
+        this.status = true
+      } else {
+        this.status = false
+      }
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'>
+input {
+  width: 318 * 100vw/360;
+  height: 30px;
+  border: none;
+  border-bottom: 3px solid black;
+  outline: none;
+  font-size: 20px;
+}
+.success {
+  border-bottom: 3px solid green;
+}
+.error {
+  border-bottom: 3px solid red;
+}
+</style>
