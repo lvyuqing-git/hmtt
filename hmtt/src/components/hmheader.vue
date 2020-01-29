9ff861a86fed730128aa0875aece3bf679163588
一打砖头拍不倒 <lyq15360562781@163.com>
2020-01-29 15:23:57 +0800

    封装信息页面头部组件

---
+++ hmtt/src/components/hmheader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+      <div class="left">
+          <van-icon name="arrow-left" />
+      </div>
+      <div class="center">{{title}}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+props: ['title'],
+}
+</script>
+
+<style scoped>
+.header{
+    display: flex;
+    height: 50px;
+    line-height: 50px;
+    margin: 0 15px;
+    color: #131313
+}
+.center {
+    margin: 0 auto
+}
+
+
+
+</style>
