b3dcc017745b7a185570a0e99519e853af81689a
一打砖头拍不倒 <lyq15360562781@163.com>
2020-01-30 15:00:54 +0800

    修改密码功能完成

--- hmtt/src/views/Edit_profile.vue
+++ hmtt/src/views/Edit_profile.vue
@@ -11,17 +11,32 @@
     </div>
     <div>
       <hmcell title="昵称"
-              :desc='user.nickname' @click="userAlter.nicknameAlter=true"></hmcell>
+              :desc='user.nickname'
+              @click="userAlter.nicknameAlter=true"></hmcell>
 
-      <van-dialog v-model="userAlter.nicknameAlter" 
+      <van-dialog v-model="userAlter.nicknameAlter"
                   title="修改昵称"
                   show-cancel-button
                   @confirm='nicknameAlter'>
-        <van-field  placeholder="请输入昵称" :value='user.nickname' ref="nick"/>
+        <van-field placeholder="请输入昵称"
+                   :value='user.nickname'
+                   ref="nick" />
       </van-dialog>
 
       <hmcell title="密码"
-              desc='******'></hmcell>
+              desc='******'
+              @click="userAlter.passwordAlter = true"></hmcell>
+
+      <van-dialog v-model="userAlter.passwordAlter"
+                  title="修改密码"
+                  show-cancel-button
+                  @confirm='passwordAlter'
+                  :before-close='passwordError'>
+        <van-field placeholder="请输入原密码"
+                   ref="oldPassword" />
+        <van-field placeholder="请输入新密码"
+                   ref="newPassword" />
+      </van-dialog>
       <hmcell title="性别"
               :desc="user.gender?'男':'女'"></hmcell>
     </div>
@@ -33,7 +48,7 @@
 import hmheader from '../components/hmheader'
 import hmcell from '../components/hmcell'
 import { uploadFile } from '../apis/apiupload'
-import { alterInfo ,userInfo} from '../apis/apiUser'
+import { alterInfo, userInfo } from '../apis/apiUser'
 export default {
   data() {
     return {
@@ -43,10 +58,10 @@ export default {
         passwordAlter: false,
         genderAlter: false
       },
-      userInfo : {
-          nickname : ''
+      userInfo: {
+        nickname: ''
       },
-      id :''
+      id: ''
     }
   },
   components: {
@@ -56,6 +71,7 @@ export default {
   async mounted() {
     let res = await userInfo(this.$route.params.id)
     this.user = res.data.data
+    console.log(this.user)
   },
   methods: {
     async afterRead(file) {
@@ -67,17 +83,47 @@ export default {
         if (res.data.data.url.indexOf('http') == -1) {
           res.data.data.url = 'http://127.0.0.1:3000' + res.data.data.url
         }
-        let res2 = await alterInfo(this.user.id, { head_img: res.data.data.url })
+        let res2 = await alterInfo(this.user.id, {
+          head_img: res.data.data.url
+        })
         if (res2.data.message == '修改成功') {
           this.user.head_img = res2.data.data.head_img
         }
       }
     },
-    async nicknameAlter(){
-        let value = this.$refs.nick.$refs.input.value
-        let res = await alterInfo(this.user.id,{nickname : value})
-        this.user.nickname = res.data.data.nickname
-        
+    async nicknameAlter() {
+      let value = this.$refs.nick.$refs.input.value
+      let res = await alterInfo(this.user.id, { nickname: value })
+      this.user.nickname = res.data.data.nickname
+    },
+    async passwordAlter() {
+      let oldPassword = this.$refs.oldPassword.$refs.input.value
+      let newPassword = this.$refs.newPassword.$refs.input.value
+      if (this.user.password == oldPassword) {
+        let res = await alterInfo(this.user.id, { password: newPassword })
+        if (res.data.message == '修改成功') {
+          this.$toast.success('修改成功')
+        }
+      } else if (oldPassword.length == 0) {
+        this.$toast.fail('请输入原密码')
+      } else {
+        this.$toast.fail('原密码错误')
+      }
+    },
+    passwordError(action, done) {
+      let oldPassword = this.$refs.oldPassword.$refs.input.value
+      //   let newPassword = this.$refs.newPassword.$refs.input.value
+      if (action == 'cancel') {
+        done()
+        return
+      }
+      if (oldPassword.length == 0) {
+        done(false)
+      } else if (oldPassword != this.user.password) {
+        done(false)
+      }else{
+        done()
+      }
     }
   }
 }
